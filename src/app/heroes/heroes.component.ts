f6cec4f4bea8e0c49898aebfe48f83b89cb1a0ba
scjeong <scjeong@humaxdigital.com>
2018-02-27 07:41:13 +0900

    Master/Detail
    Step: Add a click event binding
    Step: Add the click event handler
    Step : Update the details template
    
    See: https://angular.io/guide/template-syntax#event-binding
    
    See: #4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,12 +9,12 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {
 
