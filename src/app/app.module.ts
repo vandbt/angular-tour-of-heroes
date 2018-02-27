220630699687c4319bc2aa64c0f0e96a8b7d9b62
scjeong <scjeong@humaxdigital.com>
2018-02-27 10:09:14 +0900

    #6 : Provide the HeroService
    - provide in the AppModule using CLI when gereration time.
    - ng generate service hero --module=app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+// import Services here
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +20,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     FormsModule,
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
