6b1377407b981b29462a5619f50ff27b88a8fa33
scjeong <scjeong@humaxdigital.com>
2018-02-27 16:03:46 +0900

    #8: Heroes and HTTP - Import some HTTP symbols

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule }    from '@angular/common/http';
+
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
