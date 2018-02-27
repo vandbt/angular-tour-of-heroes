d80cf0d735c92583bcf3851f0cc9fe81e426fd53
scjeong <scjeong@humaxdigital.com>
2018-02-27 16:09:00 +0900

    #8: Heroes and HTTP - Import some HTTP symbols

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,13 +4,18 @@ import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessageService } from './message.service';
 
 
 @Injectable()
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
@@ -22,5 +27,10 @@ export class HeroService {
     // Todo: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 }
