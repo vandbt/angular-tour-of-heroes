5547f6f60aa456ff9086b7c1a47656e4c1ab65cb
scjeong <scjeong@humaxdigital.com>
2018-02-27 15:02:00 +0900

    #7: Extract the id route parameter

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -22,8 +22,15 @@ export class HeroDetailComponent implements OnInit {
     private location: Location
   ) { }
 
-  ngOnInit() {
-
+  ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    // there are no getHero method yet.
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
